f218a2533d1eda901ab64b31de5300b33f89d45c
Ariyo Joseph Olamide <midejoseph24@gmail.com>
2023-01-08 20:02:25 +0100

    Add sg

--- security_group.tf
+++ security_group.tf
@@ -2,6 +2,31 @@
 ###    Security group ###
 ########################
 
+resource "aws_security_group" "alb_security_group" {
+  name        = "alb security group"
+  description = "allow access on port 80"
+  vpc_id      = aws_default_vpc.default_vpc.id
+
+  ingress {
+    description = "http access"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "alb security group"
+  }
+}
+
 resource "aws_security_group" "ec2_security_group" {
   name        = "ec2 security group"
   description = "allow access on ports 80 and 22"
